dd04dfa24e0adae86019cb67cd4c47c8aea49171
rokib97 <rokibulhasan.ph@gmail.com>
2025-01-31 18:52:24 +0600

    code rafctor for list and tuple

--- code/chapter6/lists.py
+++ code/chapter6/lists.py
@@ -1,83 +1,69 @@
-users = ['rokib','hasan','tani']
+# Python list and tuple 
+
+# Lists
+users = ['rokib', 'hasan', 'tani']
 data = ['rokib, 42, true']
-emptylist = []
+empty_list = []
 
 # Check for a value in a list
 print("rokib" in users)
 
-
-#Retrieve a list value by index
+# Retrieve a list value by index
 print(users[0])
 print(users[-2])
 print(users.index("rokib"))
 
 # Retrieve a range of list values
-print(users[0:2])
+print(users[:2])
 print(users[1:])
 print(users[-3:-1])
 
 # Get the length of a list
 print(len(data))
 
-# Appending items to a list
+# Modifying lists
 users.append("elsa")
 users += ['saki']
-users.extend(['robert','jimmy'])
+users.extend(['robert', 'jimmy'])
 users.extend(data)
 users.insert(0, 'Bob')
 users[2:2] = ["Eddie", 'Alex']
+users[1:3] = ["Robert", 'JPJ']
 
-# Replacing list values
-users[1:3] = ["Robert",'JPJ']
-
-# Removing, deleting, clearing
+# Removing elements
 users.remove("Bob")
 users.pop()
 del users[0]
-# del data # delete entire list
-data.clear() #delete entry from list and make it empty
-
+data.clear()
 
 # Sorting lists
-users[1:2] = ['dave'] #adding lower case item 
+users[1:2] = ['dave']  # Adding a lowercase item
 users.sort()
-# users.sort(key=str.lower)
-# print(users)
+# users.sort(key=str.lower)  # Case-insensitive sorting
 
-nums = [4,43,75,1,5]
+# Working with numbers
+nums = [4, 43, 75, 1, 5]
 nums.reverse()
-# nums.sort(reverse=True)
-# print(nums)
-# print(sorted(nums, reverse=True))
-# print(nums)
+# nums.sort(reverse=True)  # Sort in descending order
 
-# copy 
+# Copying lists
 numscopy = nums.copy()
 mynums = list(nums)
 mycopy = nums[:]
 
-# print(numscopy)
-# print(mynums)
-# print(mycopy)
-# print(nums)
-
 print(type(nums))
-mylist = list([1,2,34,7])
-# print(mylist)
+mylist = list([1, 2, 34, 7])
 
+# Tuples
+mytuple = ('Dave', 42, 42, True)
+anothertuple = (1, 23, 3, 45)
 
+# Convert tuple to list, modify, and convert back
+temp_list = list(mytuple)
+temp_list.append("Neil")
+newtuple = tuple(temp_list)
 
-# tuple 
-
-mytuple = tuple(('Dave', 42, 42, True))
-anothertuple = (1,23,3,45,)
-# print(type(tuple))
-newlist = list(mytuple)
-newlist.append("Neil")
-newtuple = tuple(newlist)
-# print(newtuple)
-
-# unpack tuple 
-(one, two , *hey) = anothertuple
-print(one, two, hey)
-print(newtuple.count(42)) # how many times the number is present there
+# Unpacking tuples
+one, two, *rest = anothertuple
+print(one, two, rest)
+print(newtuple.count(42))

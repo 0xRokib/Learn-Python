c5db42db3df516d1c46ba672185d742998aec23a
rokib97 <rokibulhasan.ph@gmail.com>
2025-02-03 00:48:16 +0600

    added chapter 11 scope code refactored

--- code/chapter11/scope.py
+++ code/chapter11/scope.py
@@ -1,23 +1,42 @@
-# global scope 
-# name = "Rokib"
-# def greeting(name):
-#     color = 'blue'
-#     print(color)
-#     print(name)
-    
-# greeting('John')
+# Global Scope
+global_var = "I am global"
 
-count = 1
-def another():
-    color = 'blue'
-    global count
-    count += 1
-    def greeting(name):
-        nonlocal color
-        color = 'red'
-        print(color)
-        print(name)
-        
-    greeting("dave")
+def outer_function():
+    # Enclosing Scope
+    enclosing_var = "I am enclosing"
+
+    def inner_function():
+        # Local Scope
+        local_var = "I am local"
+        print(local_var)  # Accessing local variable
+        print(enclosing_var)  # Accessing enclosing variable
+        print(global_var)  # Accessing global variable
     
-another()
+    inner_function()
+
+outer_function()
+
+# Modifying global variable
+counter = 0
+
+def increment():
+    global counter
+    counter += 1
+    print("Counter:", counter)
+
+increment()
+increment()
+
+# Using nonlocal
+def outer():
+    enclosing_value = "blue"
+
+    def inner():
+        nonlocal enclosing_value
+        enclosing_value = "red"
+        print("Inside inner:", enclosing_value)
+
+    inner()
+    print("Inside outer:", enclosing_value)
+
+outer()
